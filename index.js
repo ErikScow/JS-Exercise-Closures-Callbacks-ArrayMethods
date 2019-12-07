d7b5e990d01f07811c0be7c85d3296f283a23252
Erik Scow <erikscow@gmail.com>
2019-12-06 19:43:50 -0800

    comlpleted firstNamesAllCaps() function

--- index.js
+++ index.js
@@ -208,8 +208,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let newList = [];
+  runners.map(function(item) {
+    newList.push(`${(item.first_name).toUpperCase()}`);
+  });
+  return newList;
 }
 
 /**
